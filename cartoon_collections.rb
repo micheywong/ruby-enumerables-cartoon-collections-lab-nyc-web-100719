6289439f7af3c3bac5a3a57322fbb11a0075c9a8
Michelle Wong <michellekwong2@gmail.com>
2019-09-27 14:58:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,9 @@ def summon_captain_planet(planeteer_calls)
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  i = 0 
+  if array 
 end
 
 def find_the_cheese# code an argument here
